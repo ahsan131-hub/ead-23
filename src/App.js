7a881cc4f6e178a62945a3b5ead258647e566abc
ahsan131-hub <mahsan.bscsf19@iba-suk.edu.pk>
2023-05-27 16:40:01 +0500

    Add UserParticipation Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import PollDisplay from "./components/PollDisplay";
+import UserParticipation from "./components/UserParticipation";
 import { useQuestions } from "./context/Questions";
 function App() {
   const questions = useQuestions();
   return (
     <div className="App">
       <PollDisplay />
+      <UserParticipation />
     </div>
   );
 }
---
+++ src/components/UserParticipation.js
@@ -0,0 +1,41 @@
+import { useQuestions } from "../context/Questions";
+import React from "react";
+
+function UserParticipation() {
+  const { questions, updateVote } = useQuestions();
+  const [submitted, setSubmitted] = React.useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const choiceId = e.target.choice.value;
+    console.log(choiceId);
+    updateVote(parseInt(choiceId));
+    setSubmitted(true);
+  };
+  return (
+    <div>
+      <div>
+        <h1>{questions.question}</h1>
+      </div>
+      <div>
+        <h2>Poll Form</h2>
+        <form onSubmit={handleSubmit}>
+          {questions.choices.map((choice) => (
+            <div key={choice.id}>
+              <input
+                type="radio"
+                id={choice.id}
+                name="choice"
+                value={choice.id}
+              />
+              <label htmlFor={choice.id}>{choice.label}</label>
+            </div>
+          ))}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default UserParticipation;
